2440015b9491d22b575f92438b61f1ea47732833
JusferK <fer123arriaga@gmail.com>
2024-03-12 16:11:55 -0600

    proyect from codecademy

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace moneymaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value you would like to convert: ");
+            String totalToString = Console.ReadLine();
+            Console.WriteLine($"{totalToString} cents is equal to...");
+            double total = Convert.ToDouble(totalToString);
+            total = Math.Floor(total);
+
+            int goldValue = 10;
+            int silverValue = 5;
+
+            double goldCoins = Math.Floor(total / goldValue);
+            double remainingAmount = total % goldValue;
+            double silverCoins = Math.Floor(remainingAmount / silverValue);
+            remainingAmount %= silverValue;
+            double bronzeCoins = remainingAmount - (remainingAmount - 1);
+
+            Console.WriteLine($"Gold coins {goldCoins} \nsilver coins {silverCoins} \nBronze coins: {bronzeCoins}");
+        }
+    }
+}
